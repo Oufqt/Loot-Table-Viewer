30279ff3c7caa29502d1895a867a0e9d016610d4
Oufqt <deadlysword30@gmail.com>
2026-06-23 20:49:45 -0400

    Keep GWD bodyguard drop rates separate

---
+++ src/test/java/com/loottableviewer/SourceNameNormalizerTest.java
@@ -0,0 +1,38 @@
+package com.loottableviewer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SourceNameNormalizerTest
+{
+    @Test
+    public void normalizeKeepsGodWarsBodyguardsSeparateFromBosses()
+    {
+        SourceNameNormalizer normalizer = new SourceNameNormalizer();
+
+        assertEquals("Sergeant Strongstack", normalizer.normalize("Sergeant Strongstack"));
+        assertEquals("Sergeant Steelwill", normalizer.normalize("Sergeant Steelwill"));
+        assertEquals("Sergeant Grimspike", normalizer.normalize("Sergeant Grimspike"));
+        assertEquals("Flight Kilisa", normalizer.normalize("Flight Kilisa"));
+        assertEquals("Flockleader Geerin", normalizer.normalize("Flockleader Geerin"));
+        assertEquals("Wingman Skree", normalizer.normalize("Wingman Skree"));
+        assertEquals("Starlight", normalizer.normalize("Starlight"));
+        assertEquals("Growler", normalizer.normalize("Growler"));
+        assertEquals("Bree", normalizer.normalize("Bree"));
+        assertEquals("Tstanon Karlak", normalizer.normalize("Tstanon Karlak"));
+        assertEquals("Zakl'n Gritch", normalizer.normalize("Zakl'n Gritch"));
+        assertEquals("Balfrug Kreeyath", normalizer.normalize("Balfrug Kreeyath"));
+    }
+
+    @Test
+    public void normalizeKeepsExistingAliases()
+    {
+        SourceNameNormalizer normalizer = new SourceNameNormalizer();
+
+        assertEquals("Chambers of Xeric", normalizer.normalize("cox"));
+        assertEquals("Theatre of Blood", normalizer.normalize("tob"));
+        assertEquals("Tombs of Amascut", normalizer.normalize("toa"));
+        assertEquals("Chest", normalizer.normalize("barrows chest"));
+    }
+}
